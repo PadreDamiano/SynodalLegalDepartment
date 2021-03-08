d7978fe97ec4f931d76fe03c54d5c996306db2fe
HOME-PC\Vitaliy <padre.vitaliy@gmail.com>
2021-03-08 22:52:54 +0300

    2

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import {Container, Navbar} from 'react-bootstrap';
+import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown, NavLink} from 'react-bootstrap';
 import logo from './logo.png';
 
 class Header extends Component {
     render() {
         return (
             <>
-                <Navbar>
+                <Navbar collapseOnSelect
+                        expand='md'
+                        bg='dark'
+                        variant='dark'>
                     <Container>
                         <Navbar.Brand href="/">
                             <img src={logo}
@@ -16,6 +19,32 @@ class Header extends Component {
                                  alt='UOC'
                             />
                         </Navbar.Brand>
+                        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                        <Navbar.Collapse id='responsive-navbar-nav'>
+                            <Nav className='mr-auto'>
+                                <NavLink href='/' >Додому</NavLink>
+                                <NavDropdown title="Єпархії" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="/diocese/kyiv">Киівська</NavDropdown.Item>
+                                    <NavDropdown.Item href="/diocese/chernigov">Чернігівська</NavDropdown.Item>
+                                    <NavDropdown.Item href="/diocese/odessa">Одеська</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Інші</NavDropdown.Item>
+                                </NavDropdown>
+                                <NavDropdown title="Області" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="/region/kyiv">Київська</NavDropdown.Item>
+                                    <NavDropdown.Item href="/region/chernigov">Чернігівська</NavDropdown.Item>
+                                    <NavDropdown.Item href="/region/odessa">Одеська</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Інші</NavDropdown.Item>
+                                </NavDropdown>
+                                <NavLink href='/observe'>Відстеження</NavLink>
+                                <NavLink href='/actual'>Актуальні засідання</NavLink>
+                            </Nav>
+                            <Form inline>
+                                <FormControl type="text" placeholder="Пошук" className=" mr-sm-2"/>
+                                <Button variant='outline-info'>Пошук</Button>
+                            </Form>
+                        </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </>
